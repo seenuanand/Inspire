71a622b17410b13f1f156ebef2544e2f45de15dd
seenuanand <srinivas.anand1@gmail.com>
2022-11-15 17:02:40 +0530

    Page Object Model Implimentation

---
+++ configuration/config.py
@@ -0,0 +1,10 @@
+class TestData:
+    BROWSER = 'chrome'
+    CHROME_EXECUTABLE_PATH = "drivers/chromedriver_mac"
+    FIREFOX_EXECUTABLE_PATH = "Drivers/geckodriver_mac"
+
+    URL = "https://www.inspire.com/"
+    USERNAME = "srinivas.anand1@gmail.com"
+    PASSWORD = "srinivas.anand1@gmail.com"
+
+    IMPLICIT_WAIT = 10
